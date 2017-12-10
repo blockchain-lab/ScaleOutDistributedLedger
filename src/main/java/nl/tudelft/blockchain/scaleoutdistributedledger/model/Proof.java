9a4c5b60bcd91ff3b29014a9275df012008c215a
Bart de Jonge <bartdejonge96@gmail.com>
2017-12-10 16:40:31 +0100

    updated proof verification with chainview

--- src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/model/Proof.java
+++ src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/model/Proof.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 import java.util.HashMap;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.OptionalInt;
 
 /**
@@ -39,10 +40,24 @@ public class Proof {
      * @return - boolean indicating if this proof is valid.
      */
     public boolean verify() {
+        return verify(this.transaction);
+    }
+
+    /**
+     * Verifies the given transaction using this proof.
+     * @param transaction - the transaction to verify
+     * @return - boolean indicating if this transaction is valid.
+     */
+    private boolean verify(Transaction transaction) {
         int absmark = 0;
         final int[] count = {0};
 
-        for (Block block : transaction.getSender().getChain().getBlocks()) {if(block.getTransactions().contains(transaction)) count[0]++;
+        ChainView chainView = new ChainView(transaction.getSender().getChain(), chainUpdates.get(transaction.getSender()));
+        if (!chainView.isValid()) return false;
+        ListIterator<Block> iterator = chainView.listIterator();
+        while(iterator.hasNext()) {
+            Block block = iterator.next();
+            if (block.getTransactions().contains(transaction)) count[0]++;
             // TODO: check block hash (hash function needed) if not genesis block
             BlockAbstract blockAbstract = block.getBlockAbstract();
             if (blockAbstract.isOnMainChain()) {
@@ -50,12 +65,14 @@ public class Proof {
                 if (!blockAbstract.checkBlockHash(block) || !blockAbstract.checkSignature()) return false;
             }
         }
+
         OptionalInt blockNumber = transaction.getBlockNumber();
         if (!blockNumber.isPresent() || absmark < blockNumber.getAsInt()) return false;
         if(count[0] != 1) return false;
 
+        // Verify source transaction
         for (Transaction sourceTransaction : transaction.getSource()) {
-            // TODO: check if sources are valid
+            if (!verify(sourceTransaction)) return false;
         }
         return true;
     }
