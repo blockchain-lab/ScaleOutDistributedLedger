ae2f358e862b574803ab12e08f47ba5f2d3c040f
Alex <alxmorais8@msn.com>
2017-12-19 15:45:00 +0100

    Add LocalStore

--- src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/Application.java
+++ src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/Application.java
@@ -1,65 +1,38 @@
 package nl.tudelft.blockchain.scaleoutdistributedledger;
 
-import lombok.Getter;
+import java.io.IOException;
 import nl.tudelft.blockchain.scaleoutdistributedledger.model.Node;
 import nl.tudelft.blockchain.scaleoutdistributedledger.model.Proof;
 import nl.tudelft.blockchain.scaleoutdistributedledger.model.RSAKey;
 import nl.tudelft.blockchain.scaleoutdistributedledger.model.Transaction;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 /**
- * Class to represent an application.
+ * Class to run a node.
  */
 public class Application {
 
 	public static final String NODE_ADDRESS = "localhost";
 	public static final int NODE_PORT = 8007;
+	
+	private LocalStore localStore;
 
-	private final Verification verification = new Verification();
-	
-	@Getter
-	private Node ownNode;
-	
-	@Getter
-	private Map<Integer, Node> nodes = new HashMap<>();
-	
-	@Getter
-	private Set<Transaction> unspent = new HashSet<>();
-	
 	/**
 	 * Creates a new application.
+	 * @throws IOException - error while registering nodes
 	 */
 	public Application() throws IOException {
-		init();
+		this.setupNode();
 	}
-	
-	/**
-	 * @param id - the id
-	 * @return the node with the given id, or null
-	 */
-	public Node getNode(int id) throws IOException {
-		Node node = nodes.get(id);
-		if (node == null) {
-			TrackerHelper.updateNodes(nodes);
-			node = nodes.get(id);
-		}
-		return node;
-	}
-	
+
 	/**
 	 * Called when we receive a new transaction.
 	 * @param transaction - the transaction
 	 * @param proof       - the proof
 	 */
 	public synchronized void receiveTransaction(Transaction transaction, Proof proof) {
-		if (CommunicationHelper.receiveTransaction(verification, transaction, proof)) {
+		if (CommunicationHelper.receiveTransaction(localStore.getVerification(), transaction, proof)) {
 			if (transaction.getAmount() > 0) {
-				unspent.add(transaction);
+				localStore.getUnspent().add(transaction);
 			}
 		}
 	}
@@ -75,14 +48,17 @@ public class Application {
 	}
 	
 	/**
-	 * Initializes this application.
+	 * Setup your own node.
+	 * Register to the tracker and setup the local store.
+	 * @throws java.io.IOException - error while registering node
 	 */
-	private void init() throws IOException {
+	private void setupNode() throws IOException {
+		// Create and register node
 		RSAKey key = new RSAKey();
-		this.ownNode = TrackerHelper.registerNode(key.getPublicKey(), "localhost", 80);
-		// TODO: set actual IP address and port
-		this.ownNode.setPrivateKey(key.getPrivateKey());
+		Node ownNode = TrackerHelper.registerNode(key.getPublicKey(), "localhost", 80);
+		ownNode.setPrivateKey(key.getPrivateKey());
 		
-		nodes.put(this.ownNode.getId(), this.ownNode);
+		// Setup local store
+		localStore = new LocalStore(ownNode);
 	}
 }
