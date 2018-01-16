a848911535ea5b6d17fb115a93d17f85b84bbd2f
Bart de Jonge <b.a.p.dejonge@student.tudelft.nl>
2018-01-16 16:06:18 +0100

    resetting tracker server on simulation start

--- src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/SimulationMain.java
+++ src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/SimulationMain.java
@@ -1,8 +1,11 @@
 package nl.tudelft.blockchain.scaleoutdistributedledger;
 
 import nl.tudelft.blockchain.scaleoutdistributedledger.simulation.Simulation;
+import nl.tudelft.blockchain.scaleoutdistributedledger.simulation.tendermint.TendermintHelper;
 import nl.tudelft.blockchain.scaleoutdistributedledger.simulation.transactionpattern.ITransactionPattern;
 import nl.tudelft.blockchain.scaleoutdistributedledger.simulation.transactionpattern.RandomTransactionPattern;
+
+import javax.sound.midi.Track;
 
 /**
  * Main class for running a simulation.
@@ -15,8 +18,13 @@ public final class SimulationMain {
 	 * @throws Exception - If an exception occurs.
 	 */
 	public static void main(String[] args) throws Exception {
-		// Clean Tendermint folder
 		//NOTE: The tracker should be started first, manually
+
+		// Clean Tendermint folder
+		TendermintHelper.cleanTendermintFiles();
+		// reset the tracker server
+		TrackerHelper.resetTrackerServer();
+
 		Simulation simulation = new Simulation();
 		ITransactionPattern itp = new RandomTransactionPattern(10, 20, 1000, 2000, 1);
 		simulation.setTransactionPattern(itp);
