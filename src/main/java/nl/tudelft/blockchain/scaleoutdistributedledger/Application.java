1b4596e936ff9df85b30d1df5a7e105146888892
Taico Aerts <info@taico.nl>
2018-01-18 22:41:06 +0100

    Don't update always

--- src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/Application.java
+++ src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/Application.java
@@ -62,7 +62,6 @@ public class Application {
 
 		// Setup local store
 		localStore = new LocalStore(ownNode, this, genesisBlock, this.isProduction);
-		localStore.updateNodes();
 		localStore.initMainChain();
 
 		serverThread = new Thread(new SocketServer(nodePort, localStore));
