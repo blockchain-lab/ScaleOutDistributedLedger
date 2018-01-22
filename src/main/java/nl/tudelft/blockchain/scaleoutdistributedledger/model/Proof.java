3a516e3fae6c18a0d8825fc38e4d09b75f33de72
Alex <alxmorais8@msn.com>
2018-01-22 14:17:03 +0100

    Decode all chains of the proof

--- src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/model/Proof.java
+++ src/main/java/nl/tudelft/blockchain/scaleoutdistributedledger/model/Proof.java
@@ -55,25 +55,31 @@ public class Proof {
 	 * @throws IOException - error while getting node info from tracker
 	 */
 	public Proof(ProofMessage proofMessage, LocalStore localStore) throws IOException {
-		this.chainUpdates = new HashMap<>();
-		// Start by decoding the chain of the sender
-		Node senderNode = localStore.getNode(proofMessage.getTransactionMessage().getSenderId());
-		List<BlockMessage> senderChain = proofMessage.getChainUpdates().get(senderNode.getId());
-		// Start from the last block
-		BlockMessage lastBlockMessage = senderChain.get(senderChain.size() - 1);
-		// Recursively decode the transaction and chainUpdates
-		Block lastBlock = new Block(lastBlockMessage, proofMessage.getChainUpdates(), this.chainUpdates, localStore);
-		List<Block> currentDecodedBlockList;
-		if (this.chainUpdates.containsKey(senderNode)) {
-			// Add to already created list of blocks
-			currentDecodedBlockList = this.chainUpdates.get(senderNode);
-			currentDecodedBlockList.add(lastBlock);
-		} else {
-			// Create new list of blocks
-			currentDecodedBlockList = new ArrayList<>();
-			currentDecodedBlockList.add(lastBlock);
-			this.chainUpdates.put(senderNode, currentDecodedBlockList);
+		this.chainUpdates = new HashMap<>();		
+		for (Map.Entry<Integer, List<BlockMessage>> entry : proofMessage.getChainUpdates().entrySet()) {
+			Node node = localStore.getNode(entry.getKey());
+			if (this.chainUpdates.containsKey(node)) {
+				// We have already decoded this chain in a previous iteration
+				continue;
+			}
+			List<BlockMessage> blockMessageList = entry.getValue();
+			// Start from the last block
+			BlockMessage lastBlockMessage = blockMessageList.get(blockMessageList.size() - 1);
+			// Recursively decode the transaction and chainUpdates
+			Block lastBlock = new Block(lastBlockMessage, proofMessage.getChainUpdates(), this.chainUpdates, localStore);
+			List<Block> currentDecodedBlockList;
+			if (this.chainUpdates.containsKey(senderNode)) {
+				// Add to already created list of blocks
+				currentDecodedBlockList = this.chainUpdates.get(senderNode);
+				currentDecodedBlockList.add(lastBlock);
+			} else {
+				// Create new list of blocks
+				currentDecodedBlockList = new ArrayList<>();
+				currentDecodedBlockList.add(lastBlock);
+				this.chainUpdates.put(senderNode, currentDecodedBlockList);
+			}
 		}
+		
 		// Set the transaction from the decoded chain
 		// TODO [possible improvement]: is the transaction always in the last block ?
 		Transaction foundTransaction = null;
